99c98106819a6b03efee33441f2c14a6726eed2f
axuanwu <1019420880@qq.com>
2016-10-22 20:49:18 +0800

    Merge branch 'master' of E:\gitshell\optimizationMethod with conflicts.

--- BBmethod/BBsearch.py
+++ BBmethod/BBsearch.py
@@ -6,44 +6,30 @@ from myfunction import *
 
 class BBsearch:
     def __init__(self):
-        self.X = np.matrix([1.0, 1, 1, 1]).T
-        self.gk = np.matrix([1.0, 1, 1, 1]).T
-        self.accuracy = 10**-8
-        self.alpha = 0
-        self.i = 0
+        self.X = np.matrix([1, 1, 1, 1]).T
         pass
 
-    @property
     def getAlpha(self):
-        return float(self.gk.T * self.gk)/float(self.gk.T * G * self.gk)
+        pass
+
 
     # 设置初始点
     def set_first(self, X):
         self.X = np.matrix(X).T
-        self.gk = f_gx(self.X)
 
     def final_solution(self):
         # 初始配置
-        self.set_first([1.0, 1, 1, 1])
-        # print i,self.getAlpha, my_function(self.X)  # 打印搜索轨迹
+        print self.X.T  # 打印搜索轨迹
         while self.next_solution():
-            pass
-            # print i,self.alpha, my_function(self.X)
+            print self.X.T
 
     def next_solution(self):
         # 迭代一次
-        # 先计算 alpha 再更新 gk
-        self.alpha = self.getAlpha
-        self.gk = f_gx(self.X)
-        print self.i, self.alpha, my_function(self.X)  # 打印收敛轨迹
-        self.i += 1
-        self.X -= self.alpha * self.gk
-        a = np.linalg.norm(self.gk)
+        self.G_fun()
+        self.g_fun()
+        self.X = self.X + self.dk_fun()
+        a = np.linalg.norm(self.gx.T)
         if a > self.accuracy:
             return True
         else:
             return False
-
-if __name__ == "__main__":
-    aa = BBsearch()
-    aa.final_solution()  # 找最优解
